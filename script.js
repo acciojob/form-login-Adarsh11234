88a7c42df1a3f33dfda9d540e2cccd103a46f43f
Adarsh11234 <aadarshpatil123@gmail.com>
2025-01-24 12:17:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-function getFormvalue() {
-    //Write your code here
 
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("form1"); 
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault(); 
+        getFormvalue();
+    });
+});
+
+function getFormvalue() {
+   
+    const firstName = document.querySelector("input[name='fname']").value;
+    const lastName = document.querySelector("input[name='lname']").value;
+
+   
+    alert(`${firstName} ${lastName}`);
 }
+
